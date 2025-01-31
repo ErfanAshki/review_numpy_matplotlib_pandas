1ae3ba777571507a0a4a0418b548759cd520d27e
erfan ashki <erfanbayern111@gmail.com>
2025-01-31 15:47:27 +0330

    math operating between array and a number

--- test.py
+++ test.py
@@ -1,16 +1,10 @@
 import numpy as np 
 
 np_array1 = np.array([-8,0,3,14])
-np_array2 = np.array([10,25,4,0])
+b = 5
 
-array3 = np_array1 + np_array2
-array4 = np_array1 - np_array2
-array5 = np_array1 * np_array2
-array6 = np_array1 / np_array2
-array7 = np_array1 ** np_array2
-
-print(array3)
-print(array4)
-print(array5)
-print(array6)
-print(array7)
+print(np_array1 + b)
+print(np_array1 - b)
+print(np_array1 * b)
+print(np_array1 / b)
+print(np_array1 % b)
