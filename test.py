73f4d2d15f3f0c39fdbd9226d27facf2213f103e
erfan ashki <erfanbayern111@gmail.com>
2025-01-31 15:05:11 +0330

    use array part 1

---
+++ test.py
@@ -0,0 +1,10 @@
+import numpy as np 
+
+my_list = [1,3,5,7]
+my_tuple = (4,8,12)
+
+np_array_list = np.array(my_list)
+np_array_tuple = np.array(my_tuple)
+
+print(np_array_list)
+print(np_array_tuple)
