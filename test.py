477570475e77a778113a81b8260ef23271b2294f
erfan ashki <erfanbayern111@gmail.com>
2025-02-01 01:05:02 +0330

    example for [: , np.newaxis]

--- test.py
+++ test.py
@@ -1,6 +1,8 @@
 import numpy as np 
 
 np_array1 = np.array([-8,0,3,14])
+np_array2 = np.array((10,20,30,40))
 new_array = np_array1[: , np.newaxis]   
 
-print(new_array)
+print(new_array + np_array2)
+    
