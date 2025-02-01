98ab0e321ba76246e95b618216b2f72317b35186
erfan ashki <erfanbayern111@gmail.com>
2025-02-01 21:58:46 +0330

    calc determinan with linalg.det method part 2

--- test.py
+++ test.py
@@ -1,7 +1,7 @@
 import numpy as np 
 
 
-array1 = np.array([[3,11] , [10,0]])                               
+array1 = np.array([[-8,3,11] , [10,0,55], [8,12,-1]])                                                             
 
 print(np.linalg.det(array1))
 
