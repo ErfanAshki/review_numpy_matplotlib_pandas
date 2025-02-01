d16b8c6e15437cc089547f36ac2631bcd977d2cf
erfan ashki <erfanbayern111@gmail.com>
2025-02-01 15:05:39 +0330

    2-dimensional & 3-dimensional array

--- test.py
+++ test.py
@@ -1,13 +1,17 @@
 import numpy as np 
 
-np_array1 = np.array([-8,0,3,14])
-np_array2 = np.array([1,5.5,3])
-np_array3 = np.array(['book','note'])
+np_array1 = np.array([[-8,0,3,14] , [10,20,30,40]])
+np_array2 = np.array([[1,5.5,3], ['unctuous', 'unction', '55'], [0,0,0]])
+np_array3 = np.array([[[1,5.5,3], ['unctuous', 'unction', '55']], [[0,0,0], [1,11,111]]])
 
-
+print(np_array1)
 print(np_array1.shape)
 print(np_array1.dtype)
-print(np_array2.shape)
-print(np_array2.dtype)
+print()
+print(np_array1)
+print(np_array1.shape)
+print(np_array1.dtype)
+print()
+print(np_array3)
 print(np_array3.shape)
 print(np_array3.dtype)
