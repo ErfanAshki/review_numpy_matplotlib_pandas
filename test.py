9d9f0e9e035b562b14ba460776adf01e6295eae0
erfan ashki <erfanbayern111@gmail.com>
2025-02-01 21:42:16 +0330

    inverse of array with linalg.inv method part 1

--- test.py
+++ test.py
@@ -1,7 +1,7 @@
 import numpy as np 
 
 
-array1 = np.array([[-8,3,11] , [10,0,55], [8,12,-1]])                               
+array1 = np.array([[-8,3,11] , [10,0,55]])                               
 
-print(np.trace(array1))
+print(np.linalg.inv(array1))
 
