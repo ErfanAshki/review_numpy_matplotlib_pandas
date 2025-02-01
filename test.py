7053e4d22bb3004f496158df1b392519857a46b8
erfan ashki <erfanbayern111@gmail.com>
2025-02-01 17:18:42 +0330

    indexing in array part 2

--- test.py
+++ test.py
@@ -3,9 +3,8 @@ import numpy as np
 
 array1 = np.array([[-8,3,11] , [10,0,55], [8,12,-1]])                               
 
-print(array1[1,1])
-print(array1[0,2])
-print(array1[2,2])
-print(array1[1,0])
-print(array1[0,0])
+print(array1[2])
+print(array1[1,])
+print(array1[0])
+
 
