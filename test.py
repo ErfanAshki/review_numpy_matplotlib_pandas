f3c57eede755a8694b8f70727714f6ed1bd22efb
erfan ashki <erfanbayern111@gmail.com>
2025-02-01 15:13:51 +0330

    reshape method

--- test.py
+++ test.py
@@ -1,17 +1,13 @@
 import numpy as np 
 
-np_array1 = np.array([[-8,0,3,14] , [10,20,30,40]])
-np_array2 = np.array([[1,5.5,3], ['unctuous', 'unction', '55'], [0,0,0]])
-np_array3 = np.array([[[1,5.5,3], ['unctuous', 'unction', '55']], [[0,0,0], [1,11,111]]])
+np_array1 = np.array([[1,3], ['unctuous', 'unction'], [0,0]])
+np_array2 = np_array1.reshape(2,3)
+np_array3 = np_array1.reshape(1,6)
 
 print(np_array1)
-print(np_array1.shape)
-print(np_array1.dtype)
 print()
-print(np_array1)
-print(np_array1.shape)
-print(np_array1.dtype)
+print(np_array2)
 print()
 print(np_array3)
-print(np_array3.shape)
-print(np_array3.dtype)
+
+
