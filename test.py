8eb5d744b8c6e9a0cda2cae062c44b286f90ca43
erfan ashki <erfanbayern111@gmail.com>
2025-02-02 17:13:26 +0330

    multiple barometer with subplot part 1

--- test.py
+++ test.py
@@ -1,19 +1,20 @@
 import matplotlib.pyplot as plt
 import numpy as np
 
-x1 = [1,4,5,3,5]
-y1 = [2,5,2,6,7]
-sizes = [10,50,5,200,100]
-plt.scatter(x1, y1, s=sizes, label='point 1', color='green', marker='o')
+x1 = np.array((1,2,3))
+y1 = np.array((6,5,4))
+plt.subplot(1,2,1)
+plt.plot(x1, y1)
 
-x2 = [0,5,7,3]
-y2 = [2,0,9,3]
-plt.scatter(x2, y2, s=40, label='point 2', color='red', marker='*')
+x2 = np.array((0,3,6))
+y2 = np.array((1,4,7))
+plt.subplot(1,2,2)
+plt.plot(x2, y2)
 
 plt.xlabel('x-axis')
 plt.ylabel('y-axis')
 plt.xlim(0,10)
 plt.ylim(0,10)
-plt.title('scatter chart')
+plt.title('subplot horizontal chart')
 plt.legend()
 plt.show()
