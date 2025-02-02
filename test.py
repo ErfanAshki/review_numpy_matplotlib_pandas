80f5d3a412ac75daaf24391f60fa8a87d1cc0cde
erfan ashki <erfanbayern111@gmail.com>
2025-02-02 13:42:43 +0330

    Three barometer in one screen & options of plot function

--- test.py
+++ test.py
@@ -1,13 +1,21 @@
 import matplotlib.pyplot as plt
 
 
-x = [-2,4,1,7]
-y = [0,3,-1,5]
+x1 = [-2,0,4]
+y1 = [0,3,6]
+plt.plot(x1,y1, label='line1', linestyle='solid', color='green', linewidth=1, marker='o', markerfacecolor='yellow', markersize='8')
 
+x2 = [2,5,6]
+y2 = [1,4,3]
+plt.plot(x2,y2, label='line2', linestyle='dashed', color='blue', linewidth=3, marker='o', markerfacecolor='yellow', markersize='6')
 
-plt.plot(x,y)
+x3 = [3,0,-1]
+y3 = [0,1,3]
+plt.plot(x3,y3, label='line3', linestyle='dotted', color='red', linewidth=5, marker='o', markerfacecolor='yellow', markersize='4')
+
 plt.xlabel('x-axis')
 plt.ylabel('y-axis')
-plt.title('barometer 2')
+plt.title('Three barometer')
+plt.legend()
 
 plt.show()
