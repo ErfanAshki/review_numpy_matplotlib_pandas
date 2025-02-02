013063efd7f40a2c0ca0fd54165ac9b4478a365f
erfan ashki <erfanbayern111@gmail.com>
2025-02-02 13:06:14 +0330

    another line barometer

--- test.py
+++ test.py
@@ -1,13 +1,13 @@
 import matplotlib.pyplot as plt
 
 
-x = [1,3,5]
-y = [2,4,6]
+x = [-2,4,1,7]
+y = [0,3,-1,5]
 
 
 plt.plot(x,y)
 plt.xlabel('x-axis')
 plt.ylabel('y-axis')
-plt.title('barometer')
+plt.title('barometer 2')
 
 plt.show()
