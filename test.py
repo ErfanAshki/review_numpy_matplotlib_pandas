59f77a7913d752e8bdf74fcfec1e73e21aaf0e05
erfan ashki <erfanbayern111@gmail.com>
2025-02-02 14:37:08 +0330

    Two bar chart in screen

--- test.py
+++ test.py
@@ -1,12 +1,17 @@
 import matplotlib.pyplot as plt
+import numpy as np
 
+x = np.arange(4)
+my_marks = [20,18,15,17]
+your_marks = [18,14,20,17]
+bar_width = 0.2
 
-x = [1,3,5,7]
-height = [10,20,30,40]
+p1 = plt.bar(x, my_marks, bar_width, color='red', label='my marks')
+p2 = plt.bar(x + bar_width, your_marks, bar_width, color='blue', label='your marks')
 
-plt.bar(x, height, width=0.5, color=['red','blue','green','yellow'], label='bar1')
-
-plt.xlabel('x-axis')
-plt.ylabel('y-axis')
-plt.title('bar chart')
+plt.xlabel('subjects')
+plt.ylabel('scores')
+plt.title('bar chart scores')
+plt.xticks(x + (bar_width/2) , ('English','Sport','Math','Science'))
+plt.legend()
 plt.show()
