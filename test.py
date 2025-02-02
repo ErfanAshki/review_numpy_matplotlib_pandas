fb10c408356d9f4e10ae54b5cf6bfe076a2ba110
erfan ashki <erfanbayern111@gmail.com>
2025-02-02 14:13:04 +0330

    bar chart

--- test.py
+++ test.py
@@ -1,23 +1,12 @@
 import matplotlib.pyplot as plt
 
 
-x1 = [-2,0,4]
-y1 = [0,3,6]
-plt.plot(x1,y1, label='line1', linestyle='solid', color='green', linewidth=1, marker='o', markerfacecolor='yellow', markersize='8')
+x = [1,3,5,7]
+height = [10,20,30,40]
 
-x2 = [2,5,6]
-y2 = [1,4,3]
-plt.plot(x2,y2, label='line2', linestyle='dashed', color='blue', linewidth=3, marker='o', markerfacecolor='yellow', markersize='6')
-
-x3 = [3,0,-1]
-y3 = [0,1,3]
-plt.plot(x3,y3, label='line3', linestyle='dotted', color='red', linewidth=5, marker='o', markerfacecolor='yellow', markersize='4')
+plt.bar(x, height, width=0.5, color=['red','blue','green','yellow'], label='bar1')
 
 plt.xlabel('x-axis')
 plt.ylabel('y-axis')
-plt.xlim(-4,10)
-plt.ylim(-2,8)
-plt.title('Three barometer')
-plt.legend()
-
+plt.title('bar chart')
 plt.show()
